20a7ef246bc12d51caf2b5199242f8fa9551769e
samuelchou <samuelchou@hotmail.com.tw>
2020-10-03 23:31:04 +0800

    <Add> 頁面一：新增文字，按鈕。按鈕按下後會開啟頁面二。

--- app/src/main/java/com/cloudinteractive/samuelchou/ui/Activity1.java
+++ app/src/main/java/com/cloudinteractive/samuelchou/ui/Activity1.java
@@ -1,18 +1,30 @@
 package com.cloudinteractive.samuelchou.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cloudinteractive.samuelchou.R;
 
 public class Activity1 extends AppCompatActivity {
-    // TODO: 2020/10/3 呈現名字及一個 Button，佔整體畫面各半之中間
-    // TODO: 2020/10/3 點擊 Button 換場到下一個⾴面
+    // COMPLETED: 2020/10/3 呈現名字及一個 Button，佔整體畫面各半之中間
+    // COMPLETED: 2020/10/3 點擊 Button 換場到下一個⾴面
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity1.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
