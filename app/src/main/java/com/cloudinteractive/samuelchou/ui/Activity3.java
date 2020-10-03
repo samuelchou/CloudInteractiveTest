eaddcc7540601d1f41bf47e9f6a64aad2e60a13a
samuelchou <samuelchou@hotmail.com.tw>
2020-10-04 00:09:54 +0800

    <Refactor> 頁面三：稍做執行順序修改，並完善安全訊息。

--- app/src/main/java/com/cloudinteractive/samuelchou/ui/Activity3.java
+++ app/src/main/java/com/cloudinteractive/samuelchou/ui/Activity3.java
@@ -3,6 +3,7 @@ package com.cloudinteractive.samuelchou.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +13,6 @@ public class Activity3 extends AppCompatActivity {
     // TODO: 2020/10/3 呈現點擊當格的id, title, thumbnailUrl 所對應的圖片
     // TODO: 2020/10/3 id, title: 文字置中呈現
     // TODO: 2020/10/3 thumbnailUrl: ImageView 呈現於背景中
-    // TODO: 2020/10/3 點擊畫面任意地方返回上一頁
 
     public static final String ARG_ID = "Arg_ID";
 
@@ -21,19 +21,21 @@ public class Activity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_3);
 
         Intent intent = getIntent();
         if (intent.getExtras() == null) {
-            // TODO: 2020/10/3 跳出訊息告知使用者 該Activity未能成功載入
+            // COMPLETED: 2020/10/3 跳出訊息告知使用者 該Activity未能成功載入
+            Toast.makeText(this, R.string.msg_fail_to_load, Toast.LENGTH_SHORT).show();
             finish();
             return;
         }
         id = intent.getIntExtra(ARG_ID, -1);
 
+        setContentView(R.layout.activity_3);
         this.findViewById(android.R.id.content).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // COMPLETED: 2020/10/3 點擊畫面任意地方返回上一頁
                 finish();
             }
         });
